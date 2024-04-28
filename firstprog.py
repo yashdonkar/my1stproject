1a204a78338800f338d102a5a9e07db43923e341
Kaushil <kaushil.nagrale@gmail.com>
2024-04-28 13:19:56 +0530

    OPPS basics start

--- firstprog.py
+++ firstprog.py
@@ -6,3 +6,24 @@ num=3
 num+=4
 print(num)
 
+def art(n):
+    print(n + " did art!!!")
+
+art(n = "Jeetada")
+
+
+# object oriented nahi ahe
+
+class Person:
+    # class attribute
+    species = "Mammal"
+
+    # Instance attribute
+    def __init__(self, name):
+        self.name = name
+        
+    def speak(self):
+        print("My name is {}".format(self.name))
+
+
+        
---
+++ oops.py
@@ -0,0 +1,23 @@
+class Person:
+    # class attribute
+    species = "Mammal"
+
+    # Instance attribute
+    def __init__(self, name, hairs):
+        self.name = name
+        self.hairs = hairs
+        
+    def speak(self):
+        print("My name is {}".format(self.name))
+    
+    def hair(self):
+        print("My hair is {}".format(self.hairs))
+
+
+a = Person("Yash","Long") 
+a.speak()
+a.hair()   
+
+b = Person("XYZ","bald")
+b.speak()
+b.hair()  
